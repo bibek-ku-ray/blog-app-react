7602b69bd076fd5f64efc0617cf89a70461a9367
bibek-ku-ray <bi.ray1225@gmail.com>
2024-08-06 05:55:28 +0545

    chore: Add Appwrite service for creating, updating, and deleting posts and files

---
+++ src/appwrite/config.js
@@ -0,0 +1,135 @@
+import { Client, Databases, ID, Query, Storage } from "appwrite";
+import conf from "../conf/config";
+
+export class Service{
+    client = new Client();
+    database;
+    bucket
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+            this.database = new Databases(this.client)
+            this.bucket = new Storage(this.client)
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}){
+        try {
+            return await this.database.createDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug, 
+                {
+                    title, 
+                    content, 
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error);
+            
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, status}){
+        try {
+            return await this.database.updateDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug, 
+                {
+                    title, 
+                    content, 
+                    featuredImage,
+                    status,
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error", error);
+            
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.database.deleteDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error", error);
+            return false;
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.database.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+            )
+            
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error", error);
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.database.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite service :: listPost :: error", error);
+            return false
+        }
+    }
+
+    // File upload service
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique,
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error);
+            return false
+        }
+    }
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error", error);
+            return false
+        }
+    }
+    async previewFile(fileId){
+        try {
+            await this.bucket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: previewFile :: error", error);
+            return false
+        }
+    }
+}
+
+const service = new Service()
