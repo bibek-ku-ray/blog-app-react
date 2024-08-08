72a1b841e1eede21d6df0dd92999f051dc68574d
bibek-ku-ray <bi.ray1225@gmail.com>
2024-08-08 23:46:25 +0545

    feat: Add Footer, Header components and Redux configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 import conf from './conf/config';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './store/authSlice';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <div>
-      <h1>
-        This is Blog app. id: {conf.appwriteProjectId}
-      </h1>
-      
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userdata) => {
+      if(userdata){
+        dispatch(login({userdata}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  })
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+         
+        </main>
+        <Footer />
+      </div>
     </div>
-  )
+  ): null
 }
 
 export default App
